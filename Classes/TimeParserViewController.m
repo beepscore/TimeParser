10b8aab78c042e3c9e97f83af1a5199ec74660dd
beepscore <support@beepscore.com>
2011-01-26 13:23:57 -0800

    In TimeParserViewController, import ISO8601DateFormatter. Add two methods, demoISODateFromString: and demoISODateAndTimeZoneFromString: to show use of ISO8601DateFormatter.

--- Classes/TimeParserViewController.m
+++ Classes/TimeParserViewController.m
@@ -7,6 +7,13 @@
 //
 
 #import "TimeParserViewController.h"
+#import "ISO8601DateFormatter.h"
+
+@interface TimeParserViewController ()
+- (void)demoISODateFromString:(NSString *)aDateString;
+- (void)demoISODateAndTimeZoneFromString:(NSString *)aDateString;
+@end
+
 
 @implementation TimeParserViewController
 
@@ -15,6 +22,46 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    NSString *myDateString = @"2011-01-26T08:37:15.747867Z";
+    [self demoISODateFromString:myDateString];
+    [self demoISODateAndTimeZoneFromString:myDateString];
+}
+
+
+- (void)demoISODateFromString:(NSString *)aDateString
+{
+    ISO8601DateFormatter *isoDateFormatter = [[ISO8601DateFormatter alloc] init];    
+	isoDateFormatter.parsesStrictly = YES;
+    
+    NSDate *date = [isoDateFormatter dateFromString:aDateString];
+    NSLog(@"date = %@", [date description]);
+    
+    [isoDateFormatter release];    
+}
+
+
+- (void)demoISODateAndTimeZoneFromString:(NSString *)aDateString
+{
+    ISO8601DateFormatter *isoDateFormatter = [[ISO8601DateFormatter alloc] init];    
+	isoDateFormatter.parsesStrictly = YES;
+    
+    // declare NSTimeZone object.  We will supply a pointer to it (i.e. &outTimeZone)
+    // as an argument to method dateFromString:timeZone:
+    NSTimeZone *outTimeZone = nil;
+    
+    // dateFromString:timeZone: returns an NSDate
+    // Also, it can change the object that &outTimeZone points to,
+    // and so have an effect similar to returning a second object.
+    NSDate *date = [isoDateFormatter dateFromString:@"2011-01-26T08:37:15.747867Z"
+                                              timeZone:&outTimeZone];
+    
+    NSLog(@"date = %@", [date description]);
+    
+    // check what dateFromString:timeZone: set outTimeZone to
+    NSLog(@"outTimeZone = %@", [outTimeZone description]);
+    
+    [isoDateFormatter release];       
 }
 
 
